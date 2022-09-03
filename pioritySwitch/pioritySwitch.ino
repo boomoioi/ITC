5247cd9ad0a8da21cb0929ac6ba59a0348b7ea0e
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-09-04 00:08:39 +0700

    Update pioritySwitch.ino

--- pioritySwitch/pioritySwitch.ino
+++ pioritySwitch/pioritySwitch.ino
@@ -1,11 +1,14 @@
-#define button1 12 //left
-#define button2 11 //middle
-#define button3 10 //right
-#define LED3 4 // red
-#define LED2 3 //yellow
-#define LED1 2 //green
+#define button1 12 // left
+#define button2 11 // middle
+#define button3 10 // right
+#define LED3 4     // red
+#define LED2 3     // yellow
+#define LED1 2     // green
 int startR = 0, startG = 0, startY = 0, subY = 0;
-
+int buttonState;
+int lastButtonState = LOW; 
+long lastDebounceTime = 0;
+long debounceDelay = 50;
 void setup()
 {
   Serial.begin(9600);
@@ -19,15 +22,15 @@ void setup()
 
 void loop()
 {
-  int cur = millis(); 
-//  Serial.println(digitalRead(button1));
-//  Serial.println(digitalRead(button2));
-//  Serial.println(digitalRead(button3));
-//  Serial.println("_______________________________________");
-//  delay(1000);
+  int cur = millis();
+  //  Serial.println(digitalRead(button1));
+  //  Serial.println(digitalRead(button2));
+  //  Serial.println(digitalRead(button3));
+  //  Serial.println("_______________________________________");
+  //  delay(1000);
   switchRed(cur);
-  switchGreen(cur);
-  switchYellow(cur);
+//  switchGreen(cur);
+//  switchYellow(cur);
 }
 
 void switchRed(int cur)
@@ -37,11 +40,10 @@ void switchRed(int cur)
     digitalWrite(LED3, LOW);
     startR = 0;
   }
-  else if (digitalRead(button1) == HIGH)
+  else if (debounce(button1) == 1)
   {
     digitalWrite(LED3, !digitalRead(LED3));
-    startR = millis(); //10,000
-    delay(200);
+    startR = millis(); // 10,000
   }
 }
 
@@ -56,7 +58,7 @@ void switchGreen(int cur)
   {
     digitalWrite(LED1, !digitalRead(LED1));
     startG = millis();
-    delay(200);
+    delay(300);
   }
 }
 
@@ -64,7 +66,7 @@ void switchYellow(int cur)
 {
   if (cur - startY <= 2000 && startY != 0)
   {
-    if ((cur - subY) > 500) 
+    if ((cur - subY) > 500)
     {
       digitalWrite(LED2, !digitalRead(LED2));
       subY = millis();
@@ -80,6 +82,28 @@ void switchYellow(int cur)
     digitalWrite(LED2, HIGH);
     startY = millis();
     subY = millis();
-    delay(200);
+    delay(300);
   }
+}
+
+int debounce(int buttonPin)
+{
+  int reading = digitalRead(buttonPin);
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState)
+  {
+    // reset the debouncingtimer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    // whatever the reading is at, it's been there for longer
+    // than the debouncedelay, so take it as the actual current state:
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+    }
+  }
+  lastButtonState = reading;
+  return buttonState;
 }
