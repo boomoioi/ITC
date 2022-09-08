d66c0392087b29c495d67d878e96f920dffa9e61
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-09-08 11:19:20 +0700

    Update sketch_sep08a.ino

--- week5/sketch_sep08a/sketch_sep08a.ino
+++ week5/sketch_sep08a/sketch_sep08a.ino
@@ -1,28 +1,36 @@
-#define a
-#define b
-#define c
-#define d
-#define e
-#define f
-#define g
-#define dp
+#define a 5
+#define b 4
+#define c 9
+#define d 10
+#define e 11
+#define f 6
+#define g 7
+#define dp 8
+#define SA 13
+#define SB 12
+int bitPattern = B11111001;
+int number[10] = {B01000000, B01111001, B00100100, B00110000, B00011001, B00010010, B00000010, B01111000, B00000000, B00010000};
 const int segPin[8] = {a,b,c,d,e,f,g,dp};
+
+
 void setup() {
+  Serial.begin(9600);
+  pinMode(SA, INPUT);
+  pinMode(SB, INPUT);
   for(int i=0; i<8; i++){
     pinMode(segPin[i], OUTPUT);
   }
  }
 
- void loop() {
-  sensorValue= analogRead(sensorPin);
-  // read the value from the sensor:
-  Serial.println(digitalRead(2));
-  Serial.println(sensorValue);
-  Serial.println("__________________________________________________");
-  // turn the ledPinon
-  
-  digitalWrite(ledPin, HIGH);
-  delay(sensorValue);
-  digitalWrite(ledPin, LOW);
-  delay(sensorValue);
- }
+void loop() {
+//  boolean isBitSet;
+//  for(int i=0; i<10; i++){
+//    for (int segment = 0; segment < 8; segment++){  
+//      isBitSet= bitRead(number[i], segment);
+//      digitalWrite(segPin[segment], isBitSet);  
+//    } 
+//    delay(1000);
+//  }
+
+  Serial.println(digitalRead(SB));
+} 
