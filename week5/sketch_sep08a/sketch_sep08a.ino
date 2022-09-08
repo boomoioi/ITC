d451427c98912960538b68003d470c199f97e38e
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-09-08 11:36:50 +0700

    Update sketch_sep08a.ino

--- week5/sketch_sep08a/sketch_sep08a.ino
+++ week5/sketch_sep08a/sketch_sep08a.ino
@@ -8,7 +8,13 @@
 #define dp 8
 #define SA 13
 #define SB 12
-int bitPattern = B11111001;
+int buttonState, i=0;
+int lastButtonState= LOW; 
+long lastDebounceTime= 0;
+long debounceDelay= 50;
+int buttonStateB;
+int lastButtonStateB= LOW; 
+long lastDebounceTimeB= 0;
 int number[10] = {B01000000, B01111001, B00100100, B00110000, B00011001, B00010010, B00000010, B01111000, B00000000, B00010000};
 const int segPin[8] = {a,b,c,d,e,f,g,dp};
 
@@ -20,17 +26,44 @@ void setup() {
   for(int i=0; i<8; i++){
     pinMode(segPin[i], OUTPUT);
   }
- }
+}
 
 void loop() {
-//  boolean isBitSet;
-//  for(int i=0; i<10; i++){
-//    for (int segment = 0; segment < 8; segment++){  
-//      isBitSet= bitRead(number[i], segment);
-//      digitalWrite(segPin[segment], isBitSet);  
-//    } 
-//    delay(1000);
-//  }
+  boolean isBitSet;
+  int reading = digitalRead(SA);
+  if (reading != lastButtonState) {
+    lastDebounceTime= millis();
+  }
+  if ((millis()   - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState= reading;  
+      if(buttonState == HIGH){
+        i++;
+        i = i%10;
+      }
+    }
+  }
+  lastButtonState= reading;
 
-  Serial.println(digitalRead(SB));
+  int readingB = digitalRead(SB);
+  if (readingB != lastButtonStateB) {
+    lastDebounceTimeB= millis();
+  }
+  if ((millis()   - lastDebounceTimeB) > debounceDelay) {
+    if (readingB != buttonStateB) {
+      buttonStateB= readingB;  
+      if(buttonStateB == HIGH){
+        i++;
+        i = i%10;
+      }
+    }
+  }
+  lastButtonStateB= readingB;
+  
+
+  Serial.println(i);
+  for (int segment = 0; segment < 8; segment++){  
+    isBitSet= bitRead(number[i], segment);
+    digitalWrite(segPin[segment], isBitSet);  
+  } 
 } 
