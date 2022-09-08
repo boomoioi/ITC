d33c28c43b9bdfe4e750edc46c446fbda7f9dcf8
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-09-08 11:47:21 +0700

    Update sketch_sep08a.ino

--- week5/sketch_sep08a/sketch_sep08a.ino
+++ week5/sketch_sep08a/sketch_sep08a.ino
@@ -15,7 +15,9 @@ long debounceDelay= 50;
 int buttonStateB;
 int lastButtonStateB= LOW; 
 long lastDebounceTimeB= 0;
-int number[10] = {B01000000, B01111001, B00100100, B00110000, B00011001, B00010010, B00000010, B01111000, B00000000, B00010000};
+int randomNo;
+int number[10] = {B11000000, B11111001, B10100100, B10110000, B10011001, B10010010, B10000010, B11111000, B10000000, B10010000};
+int WL[2] = {B11000010, B11110001};
 const int segPin[8] = {a,b,c,d,e,f,g,dp};
 
 
@@ -23,6 +25,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(SA, INPUT);
   pinMode(SB, INPUT);
+  randomSeed(analogRead(A0));
+  randomNo= random(0, 9);
   for(int i=0; i<8; i++){
     pinMode(segPin[i], OUTPUT);
   }
@@ -40,6 +44,10 @@ void loop() {
       if(buttonState == HIGH){
         i++;
         i = i%10;
+        for (int segment = 0; segment < 8; segment++){  
+          isBitSet= bitRead(number[i], segment);
+          digitalWrite(segPin[segment], isBitSet);  
+        } 
       }
     }
   }
@@ -53,17 +61,26 @@ void loop() {
     if (readingB != buttonStateB) {
       buttonStateB= readingB;  
       if(buttonStateB == HIGH){
-        i++;
-        i = i%10;
+        if(i>randomNo){
+          for (int segment = 0; segment < 8; segment++){  
+            isBitSet= bitRead(WL[0], segment);
+            digitalWrite(segPin[segment], isBitSet);  
+          } 
+        } else if(i<randomNo){
+          for (int segment = 0; segment < 8; segment++){  
+            isBitSet= bitRead(WL[1], segment);
+            digitalWrite(segPin[segment], isBitSet);  
+          } 
+        } else {
+          for (int segment = 0; segment < 8; segment++){  
+            isBitSet= bitRead(number[0], segment);
+            digitalWrite(segPin[segment], isBitSet);  
+            randomNo= random(0, 9);
+          } 
+        }
       }
     }
   }
   lastButtonStateB= readingB;
-  
 
-  Serial.println(i);
-  for (int segment = 0; segment < 8; segment++){  
-    isBitSet= bitRead(number[i], segment);
-    digitalWrite(segPin[segment], isBitSet);  
-  } 
 } 
