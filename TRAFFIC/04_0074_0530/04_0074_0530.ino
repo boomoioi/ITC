5a3036fa6d76ff7c8b6228c7e7727eb17e946d86
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-10-06 10:53:16 +0700

    Update 04_0074_0530.ino

--- TRAFFIC/04_0074_0530/04_0074_0530.ino
+++ TRAFFIC/04_0074_0530/04_0074_0530.ino
@@ -17,11 +17,13 @@
 #define goW 2
 #define waitW 3
 #define goK 4
-#define redKF 5
-#define blackKF 6
-#define redKS 7
-#define blackKS 8
-#define redKT 9
+#define blackKF 5
+#define greenKF 6
+#define blackKS 7
+#define greenKS 8
+#define blackKT 9
+#define greenKT 10
+
 
 #define greenTime 1000
 #define yellowTime 500
@@ -29,7 +31,7 @@
 
 int lights[8] = {LEDSG, LEDSY, LEDSR, LEDWG, LEDWY, LEDWR, LEDKG, LEDKR};
 int state = 0, isBitSet;
-int inp1, inp2, inp3, temp;
+int inp1, inp2, inp3, temp, lastTemp=0;
 
 struct State
 {
@@ -38,16 +40,17 @@ struct State
     unsigned long Next[10];
 };
 typedef const struct State SType;
-SType FSM[10] = {
+SType FSM[11] = {
     {B01011110, greenTime, {goS, waitS, waitS, waitS, goS, waitS, waitS, waitS}},
     {B01011101, yellowTime, {goW, goK, goW, goW, goW, goK, goW, goW}},
     {B01110011, greenTime, {goW, waitW, goW, waitW, waitW, waitW, waitW, waitW, waitW}},
-    {B01101011, yellowTime, {goS, goK, goK, goK, goS, goK, goS, goK}},
-    {B10011011, greenTime, {goK, goK, redKF, redKF, redKF, redKF, redKF, redKF}},
-    {B10011011, blinkTime, {blackKF, blackKF, blackKF, blackKF, blackKF, blackKF, blackKF, blackKF}},
-    {B11011011, blinkTime, {redKS, redKS, redKS, redKS, redKS, redKS, redKS, redKS}},
+    {B01101011, yellowTime, {goK, goK, goK, goK, goS, goK, goS, goK}},
+    {B10011011, greenTime, {goK, goK, blackKF, blackKF, blackKF, blackKF, blackKF, blackKF}},
+    {B11011011, blinkTime, {greenKF, greenKF, greenKF, greenKF, greenKF, greenKF, greenKF, greenKF}},
     {B10011011, blinkTime, {blackKS, blackKS, blackKS, blackKS, blackKS, blackKS, blackKS, blackKS}},
-    {B11011011, blinkTime, {redKT, redKT, redKT, redKT, redKT, redKT, redKT, redKT}},
+    {B11011011, blinkTime, {greenKS, greenKS, greenKS, greenKS, greenKS, greenKS, greenKS, greenKS}},
+    {B10011011, blinkTime, {blackKT, blackKT, blackKT, blackKT, blackKT, blackKT, blackKT, blackKT}},
+    {B11011011, blinkTime, {greenKT, greenKT, greenKT, greenKT, greenKT, greenKT, greenKT, greenKT}},
     {B10011011, blinkTime, {goS, goS, goW, goW, goS, goS, goS, goS}}
 };
 
@@ -79,6 +82,16 @@ void loop()
     inp2 = digitalRead(BUTTONW);
     inp3 = digitalRead(BUTTONK);
     temp = inp1*4 + inp2*2 + inp3;
+    if (state == 1 || state == 3 || state >= 5){
+      if (lastTemp != 0){
+         temp = lastTemp;
+      }
+    } 
+    
+    if (temp != 0){
+      lastTemp = temp;
+    }
+    
     Serial.println(temp);
     state = FSM[state].Next[temp];
 }
