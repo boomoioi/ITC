84afdeb8eb6394bbf0c6c798e913ba47249cc136
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2022-10-20 11:21:51 +0700

    Update networkCommunication.ino

--- networkCommunication/networkCommunication.ino
+++ networkCommunication/networkCommunication.ino
@@ -1,32 +1,26 @@
 #include <Wire.h>
 #define SLAVE_ADDR1 8
 char messageR[300];
+char empty[10] = "Token#0#";
 int mess = 0;
 
 void setup() {
   // Initialize I2C communications as Master
   Serial.begin(9600);
   Wire.begin();
+  setNormal();
 }
 
 void getChar(){
-  if(Serial.available() > 0){
-    while (Serial.available() > 0){
-      char inByte = Serial.read();
-      if(inByte != '\n'){
-        messageR[mess] = (char)inByte;
-      } else {
-        messageR[mess] = '\0';
-      }
+  while (Serial.available() > 0){
+    char inByte = Serial.read();
+    if(inByte != '\n'){
+      messageR[mess] = (char)inByte;
       mess++;
+    } else {
+      messageR[mess] = '\0';
+      mess = 0;
     }
-  } 
-  else {
-    char* normal = "Token#0#";
-    for(int i=0; i<8; i++){
-      messageR[i] = normal[i];
-    }
-    messageR[8]='\0';
   }
 }
 
@@ -43,26 +37,31 @@ void request(){
       i++;
     }
   }
-//  Serial.println(messageR);
+  Serial.println(messageR);
   messageR[0] = '\0';
+}
+
+void setNormal(){
+  for(int i=0; i<8; i++){
+    messageR[i] = empty[i];
+  }
+  messageR[8] = '\0';
+  
 }
 
 
 void loop() {
   
   getChar();
-  Serial.println(messageR);
-  delay(2000);
-//  if(messageR[0] != '\0'){
-//    Wire.beginTransmission(SLAVE_ADDR1);
-//    Wire.write(messageR);
-//    Wire.endTransmission();
-//    messageR[0] = '\0';
-//  }
-//  delay(100);
-//  Wire.requestFrom(8, 100);    // request 6 bytes from slave device #8
-//  request();
-//  Serial.print(messageR);
-//  delay(100);
+  delay(1000);
+  if(messageR[0] != '\0'){
+    Wire.beginTransmission(SLAVE_ADDR1);
+    Wire.write(messageR);
+    Wire.endTransmission();
+    messageR[0] = '\0';
+  }
+  delay(1000);
+  Wire.requestFrom(8, 100);    // request 6 bytes from slave device #8
+  request();
   
 }
